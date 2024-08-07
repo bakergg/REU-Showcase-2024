4e2671739ef7396b8fba0e6523888f1b7295842c
Gianna Baker <bakergg@washjeff.edu>
2024-08-07 15:45:41 -0400

    Added First Demo

---
+++ 02SportsAnalyticsDemo.jl
@@ -0,0 +1,11 @@
+### A Pluto.jl notebook ###
+# v0.19.45
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ d6a6e530-54f4-11ef-2b29-7187bf5d57d3
+
+
+# ╔═╡ Cell order:
+# ╠═d6a6e530-54f4-11ef-2b29-7187bf5d57d3
